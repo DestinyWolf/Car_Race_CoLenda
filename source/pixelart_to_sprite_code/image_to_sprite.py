06e228ee83b2138c0c2e173349192976a2f50399
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-07-12 08:27:14 +1000

    refactor: rename and refactor the main file code

--- source/pixelart_to_sprite_code/image_to_sprite.py
+++ source/pixelart_to_sprite_code/image_to_sprite.py
@@ -1,19 +1,15 @@
 from numpy import asarray
 from PIL import Image
 
+img_name = input("image name: ")
+file_name = input("file name destiny: ");
 
-img = Image.open("image2.png")
+img = Image.open(f"{img_name}.png")
 
 rgb_img = img.convert('RGB')
 
 len = img.size
 
-if len[0] == len[1]:
-    print("A imagem é quadrada")
-    
-else:
-    print("A imagem não é quadrada")
-    exit(1)
 
 sizex = len[0]//20
 sizey = len[1]//20
@@ -23,9 +19,10 @@ px_rgb = rgb_img.load()
 
 cores = {}
 
-with open("sprite.txt", "w") as arquivo:
+with open(f"{file_name}.txt", "w") as arquivo:
     arquivo.write('color_t pixeis[400];\n')
 
+count = 0;
 for i in range(0, 20):
     for j in range(0, 20):
 
@@ -39,16 +36,17 @@ for i in range(0, 20):
             g = int(round(((px_rgb[(j*sizey + sizey//2), (i*sizex + sizex//2)][1])*(7/254)),0))
             b = int(round(((px_rgb[(j*sizey + sizey//2), (i*sizex + sizex//2)][2])*(7/254)),0))
 
-        cor = "".join(f"cor{(i*20)+j}")
+        cor = "".join(f"cor{count}")
         rgb = "".join(f"{r} {g} {b}")
 
         if cores.get(rgb):
-            instrucao = f"pixeis[{(i*20) + j}] = {cores.get(rgb)};\n\n"
+            instrucao = f"pixeis[{(i*20) + j}] = {cores.get(rgb)};\n"
         else:
+            count +=1
             cores[rgb] = cor
-            instrucao = f"color_t cor{(i*20)+j};\n\ncor{(i*20)+j}.blue = {b};\ncor{(i*20)+j}.green = {g};\ncor{(i*20)+j}.red = {r};\npixeis[{(i*20)+j}] = cor{(i*20)+j};\n\n"
+            instrucao = f"color_t cor{count} = {{{b}, {g}, {r}}};\npixeis[{(i*20)+j}] = cor{count};\n"
 
-        with open("sprite.txt", "a") as arquivo:
+        with open(f"{file_name}.txt", "a") as arquivo:
             arquivo.write(instrucao)
 
         print(f"R: {r}\tG: {g}\t B:{b}")
