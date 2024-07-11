08521349695016a39ca9e59ae771eb4777eec181
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-07-10 23:03:03 -0300

    fix: change the colision module

--- source/Testes/colision_module.c
+++ source/Testes/colision_module.c
@@ -1,36 +1,32 @@
 #include "../Lib/colenda.h"
 #include "colision_module.h"
-
-//colisão entre player e bloco
-int check_colision_player_block(sprite_t player, background_block_movel_t block) {
-
-    //colisão detectada
-    if (((player.coord_x + SPRITE_SIZE >= (block.coord_x * 8)) || (player.coord_x - SPRITE_SIZE <= (block.coord_x * 8) + BLOCK_SIZE)) && (player.coord_y + SPRITE_SIZE <= (block.coord_y * 8) + BLOCK_SIZE)) {
-        return 1;
-    }
-    return 0;
-}
+#include "obstacle.h"
 
 //colisão entre player e objeto
-int check_colision_player_sprite(sprite_t player, sprite_t obj) {
-    if(((player.coord_x + SPRITE_SIZE >= obj.coord_x - SPRITE_SIZE) || (player.coord_x - SPRITE_SIZE <= obj.coord_x + SPRITE_SIZE)) && (player.coord_y + SPRITE_SIZE <= obj.coord_y - SPRITE_SIZE)) {
+int check_colision_player(sprite_t player, obstacle_t obstacle) {
+    if(obstacle.type == 0) {
+        if (((player.coord_x + 10 >= (obstacle.coord_x * 8) - (obstacle.size*8)) || (player.coord_x - 10 <= (obstacle.coord_x * 8) + (obstacle.size * 8))) && ((player.coord_y - 10 >= (obstacle.coord_y * 8) + (obstacle.size * 8 - 1)) && (player.coord_y + 10 >= obstacle.coord_y * 8 - (obstacle.size * 8 - 1)))) {
+            return 1;
+        }
+    } else {
+        if(((player.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (player.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((player.coord_y - 10 >= obstacle.coord_y + obstacle.size) && (player.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
         return 1;
+        }
     }
     return 0;
 }
 
 //colisão entre tiro e objeto
-int check_colision_shot_sprite(sprite_t bullet, sprite_t obj) {
-    if(((bullet.coord_x + SPRITE_SIZE >= obj.coord_x - SPRITE_SIZE) || (bullet.coord_x - SPRITE_SIZE <= obj.coord_x + SPRITE_SIZE)) && (bullet.coord_y + SPRITE_SIZE <= obj.coord_y - SPRITE_SIZE)) {
-        return 1;
-    }
-    return 0;
-}
+int check_colision_bullet(sprite_t bullet, obstacle_t obstacle) {
 
-//colisão entre tiro e bloco
-int check_colision_shot_block(sprite_t bullet, sprite_t obj) {
-    if (((bullet.coord_x + SPRITE_SIZE >= (block.coord_x * 8)) || (bullet.coord_x - SPRITE_SIZE <= (block.coord_x*8) + BLOCK_SIZE)) && (bullet.coord_y + SPRITE_SIZE <= (block.coord_y * 8) + BLOCK_SIZE)) {
+    if(obstacle.type == 0) {
+        if (((bullet.coord_x + 10 >= (obstacle.coord_x * 8) - (obstacle.size * 8 - 1)) || (bullet.coord_x - 10 <= (obstacle.coord_x * 8) + (obstacle.size * 8 - 1))) && ((bullet.coord_y - 10 >= (obstacle.coord_y * 8) + (obstacle.size * 8 - 1)) && (bullet.coord_y + 10 >= obstacle.coord_y * 8 - (obstacle.size * 8 - 1)))) {
+            return 1;
+        }
+    } else {
+        if(((bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((bullet.coord_y - 10 >= obstacle.coord_y + obstacle.size) && (bullet.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
         return 1;
+        }
     }
     return 0;
 }
--- source/Testes/colision_module.h
+++ source/Testes/colision_module.h
@@ -2,13 +2,10 @@
 #define COLISION_MODULE_COLENDA
 
 #include "../Lib/colenda.h"
+#include "obstacle.h"
 
-#define SPRITE_SIZE 10
-#define BLOCK_SIZE 8
+int check_colision_player(sprite_t player, obstacle_t obstacle);
+int check_colision_bullet(sprite_t bullet, obstacle_t obstacle);
 
-int check_colision_player_block(sprite_t player, background_block_movel_t block);
-int check_colision_player_sprite(sprite_t player, sprite_t obj);
-int check_colision_shot_sprite(sprite_t bullet, sprite_t obj);
-int check_colision_shot_block(sprite_t bullet, sprite_t obj);
 
 #endif
