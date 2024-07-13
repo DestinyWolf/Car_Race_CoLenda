d57471ea98a7015bb2716f351a0aba3fcd33c6fc
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-07-12 21:16:27 -0300

    fix: switch to current struct for obstacle

--- source/Testes/colision_module.c
+++ source/Testes/colision_module.c
@@ -3,29 +3,21 @@
 
 //colisão entre player e objeto
 int check_colision_player(sprite_t player, obstacle_t obstacle) {
-    if(obstacle.type == 0) {
-        if (((player.coord_x + 10 >= (obstacle.coord_x * 8) - (obstacle.size*8)) || (player.coord_x - 10 <= (obstacle.coord_x * 8) + (obstacle.size * 8))) && ((player.coord_y - 10 <= (obstacle.coord_y * 8) + (obstacle.size * 8 - 1)) && (player.coord_y + 10 >= obstacle.coord_y * 8 - (obstacle.size * 8 - 1)))) {
-            return 1;
-        }
-    } else {
-        if(((player.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (player.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((player.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (player.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
-        return 1;
-        }
+    
+    if(((player.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (player.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((player.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (player.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
+    return 1;
     }
+    
     return 0;
 }
 
 //colisão entre tiro e objeto
 int check_colision_bullet(sprite_t bullet, obstacle_t obstacle) {
 
-    if(obstacle.type == 0) {
-        if (((bullet.coord_x + 10 >= (obstacle.coord_x * 8) - (obstacle.size * 8 - 1)) || (bullet.coord_x - 10 <= (obstacle.coord_x * 8) + (obstacle.size * 8 - 1))) && ((bullet.coord_y - 10 >= (obstacle.coord_y * 8) + (obstacle.size * 8 - 1)) && (bullet.coord_y + 10 <= obstacle.coord_y * 8 - (obstacle.size * 8 - 1)))) {
-            return 1;
-        }
-    } else {
-        if(((bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((bullet.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (bullet.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
-        return 1;
-        }
+    
+    if(((bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((bullet.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (bullet.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
+    return 1;
     }
+    
     return 0;
 }
