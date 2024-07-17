018d11497f05d364f74ede53ad0ec675652355e8
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-07-17 18:22:08 -0300

    fix: solve a bug in colision moment

--- source/Testes/colision_module.c
+++ source/Testes/colision_module.c
@@ -1,23 +1,28 @@
 #include "../Lib/colenda.h"
 #include "colision_module.h"
 
-//colisão entre player e objeto
+
 int check_colision_player(sprite_t player, obstacle_t obstacle) {
     
-    if(((player.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (player.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((player.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (player.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
-    return 1;
+    /*caso alguma parte da area do jogador esteja dentro da area do objeto*/
+    if((((player.coord_x + 10 >= obstacle.coord_x - obstacle.size) && (player.coord_x - 10 <= obstacle.coord_x + obstacle.size)) || ((player.coord_x - 10 <= obstacle.coord_x + obstacle.size) && (player.coord_x + 10 >= obstacle.coord_x - obstacle.size))) && ((player.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (player.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
+        
+        return 1; /*retorna 1 indicando que houve uma colisão*/
     }
     
-    return 0;
+    return 0;     /*caso contrario, retorna 0*/
 }
 
-//colisão entre tiro e objeto
+
 int check_colision_bullet(sprite_t bullet, obstacle_t obstacle) {
 
-    
-    if(((bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size) || (bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size)) && ((bullet.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (bullet.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
-    return 1;
+    /*caso alguma parte da area do tiro esteja dentro da area do obstaculo*/
+    if((((bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size) && (bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size)) || ((bullet.coord_x - 10 <= obstacle.coord_x + obstacle.size) && (bullet.coord_x + 10 >= obstacle.coord_x - obstacle.size))) && ((bullet.coord_y - 10 <= obstacle.coord_y + obstacle.size) && (bullet.coord_y + 10 >= obstacle.coord_y - obstacle.size))) {
+        
+        /*retorna 1, indicando que houve uma colisão*/
+        return 1;
     }
     
+    /*caso contrario, retorna 0*/
     return 0;
 }
