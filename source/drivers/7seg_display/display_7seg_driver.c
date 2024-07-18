d829e01dfa78c329ea90761542fe64cd20d58537
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-07-18 13:28:00 -0300

    fix: memory mapping of displays

--- source/drivers/7seg_display/display_7seg_driver.c
+++ source/drivers/7seg_display/display_7seg_driver.c
@@ -13,7 +13,7 @@
 /* Meta information*/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Brenda Barbosa, Camila Boa Morte, Maike de Oliveira");
-MODULE_DESCRIPTION("Um módule kernel para realizar a comunicação com os displays de 7 segmentos na FPGA");
+MODULE_DESCRIPTION("Um módulo kernel para realizar a comunicação com os displays de 7 segmentos na FPGA");
 
 /*Driver data*/
 static struct
@@ -119,15 +119,15 @@ static int __init display_7seg_init(void){
   }
 
   /*Mapeando lightweight HPS-to-FPGA brigde*/
-  display_7seg.LW_virtual = ioremap(LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
+  display_7seg_data.LW_virtual = ioremap(LW_BRIDGE_BASE, LW_BRIDGE_SPAN);
 
   /*Mapeando os endereços dos 6 displays*/
-  display_7seg.hex0_ptr = (int*) (display_7seg.LW_virtual + HEX0_BASE);
-  display_7seg.hex1_ptr = (int*) (display_7seg.LW_virtual + HEX1_BASE);
-  display_7seg.hex2_ptr = (int*) (display_7seg.LW_virtual + HEX2_BASE);
-  display_7seg.hex3_ptr = (int*) (display_7seg.LW_virtual + HEX3_BASE);
-  display_7seg.hex4_ptr = (int*) (display_7seg.LW_virtual + HEX4_BASE);
-  display_7seg.hex5_ptr = (int*) (display_7seg.LW_virtual + HEX5_BASE);
+  display_7seg_data.hex0_ptr = (int*) (display_7seg_data.LW_virtual + HEX0_BASE);
+  display_7seg_data.hex1_ptr = (int*) (display_7seg_data.LW_virtual + HEX1_BASE);
+  display_7seg_data.hex2_ptr = (int*) (display_7seg_data.LW_virtual + HEX2_BASE);
+  display_7seg_data.hex3_ptr = (int*) (display_7seg_data.LW_virtual + HEX3_BASE);
+  display_7seg_data.hex4_ptr = (int*) (display_7seg_data.LW_virtual + HEX4_BASE);
+  display_7seg_data.hex5_ptr = (int*) (display_7seg_data.LW_virtual + HEX5_BASE);
 
   pr_info("%s: initialized!\n",DRIVER_NAME);
   return 0;
