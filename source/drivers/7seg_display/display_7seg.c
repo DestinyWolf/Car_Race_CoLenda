cf5b6ee12d1dae9635d29d4a4f6f07b09a04af6e
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-07-24 22:30:47 -0300

    fix: updating 7 seg display files

--- source/drivers/7seg_display/display_7seg.c
+++ source/drivers/7seg_display/display_7seg.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-#include<sys/ioctl.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <stdint.h>
 #include "display_7seg_driver.h"
 
-#define DRIVER_PATH "/dev/display_7seg"
-#define OFF 10
+#define DRIVER_PATH_7SEG "/dev/display_7seg"
+
+#define OFF 10 //index at the segment code array
+
+/* Letras*/
+#define P 12 //0001100
+#define A 8  //0001000
+#define U 65 //1000001
+#define S 18 //0010010
+#define E 6  //0000110
+
 /*
 ZERO 64 //1000000
 ONE 121 //1111001
@@ -25,6 +34,7 @@ OFF 127 //1111111
 */
 static const uint8_t segment_codes[11] = {64, 121, 36, 48, 25, 18, 2, 120, 0, 16, 127};
 
+
 /* Identificadores dos displays*/
 #define HEX0 0
 #define HEX1 1
@@ -34,20 +44,24 @@ static const uint8_t segment_codes[11] = {64, 121, 36, 48, 25, 18, 2, 120, 0, 16
 #define HEX5 5
 
 /* Variáveis globais*/
-static int dev = -1;
+static int dev_7seg = -1;
 
 /*Protótipos das funções*/
 int display_open();
-int display_write(uint8_t, uint8_t);
-int display_write_all(uint8_t*);
+int display_write_digit(uint8_t, uint8_t);
+int display_write_int(uint32_t);
+int display_write_score(uint16_t, uint8_t);
 int display_close();
 void display_clear();
+int display_write_letter(uint8_t *);
+
+
 
 int display_open(){
 
     /* abrir o arquivo com permissão de escrita e caso exista, sobreescreve o arquivo */
-    dev = open(DRIVER_PATH, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (dev == -1) {
+    dev_7seg = open(DRIVER_PATH_7SEG, O_WRONLY);
+    if (dev_7seg == -1) {
         printf("Failed to open file!\n");
         return -1;
     }
@@ -60,7 +74,7 @@ int display_write_digit(uint8_t hex, uint8_t data){
     
     struct ioctl_args args = {hex, segment_codes[data]};
 
-    ioctl(dev, WR_VALUE, &args);
+    ioctl(dev_7seg, WR_VALUE, &args);
     return 0;
 }
 
@@ -74,7 +88,7 @@ int display_write_int(uint32_t data){
     for (size_t i = 0; i < 6; i++){
         args.hex_id = i;
         args.data = segment_codes[data % 10];
-        ioctl(dev, WR_VALUE, &args);
+        ioctl(dev_7seg, WR_VALUE, &args);
         data /= 10;
     }
     return 0;
@@ -86,28 +100,38 @@ void display_clear(){
 
     for (size_t i = 0; i < 6; i++){
         args.hex_id = i;
-        ioctl(dev, WR_VALUE, &args);
+        ioctl(dev_7seg, WR_VALUE, &args);
     }
+}
+
+int display_write_score(uint16_t score, uint8_t player){ 
+    if(score > 999) return -1;
+
+    int display = player ? 3 : 0; //player -> 0 (player 1)
+
+    for (size_t i = 0; i < 3; i++){
+        display_write_digit(display++, (score % 10));
+        score /= 10;
+    }
+         
+    return 0;
 }
 int display_close(){
     /* caso haja algum erro ao encerrar a comunicação retorna -1 */ 
-    if (close(dev) == -1) {
+    if (close(dev_7seg) == -1) {
         printf("Failed to close file!\n");
         return -1;
     }
     return 0;
 }
 
-int display_write_score(uint16_t score, uint8_t player){ 
+int display_write_word(uint8_t data[]){
+    struct ioctl_args args;
 
-    if(score > 999) return -1;
-
-    int display = player ? 3 : 0; //player -> 0 (player 1)
-
-    for (size_t i = 0; i < 3; i++){
-        display_write(display++, segment_codes[data % 10]);
-        data /= 10;
+    for(size_t i = 0; i<6; i++){
+        args.hex_id = 5-i;
+        args.data = data[i];
+        ioctl(dev_7seg, WR_VALUE, &args);
     }
-         
-    return 0;
+	return 0;
 }
