45041733951f4ceb5559770a47160d5de1caef32
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-07-29 00:54:29 -0300

    refactor: following the c guidelines

--- source/drivers/pushbuttons/keys.c
+++ source/drivers/pushbuttons/keys.c
@@ -16,16 +16,18 @@
 #define BUTTON3 '3'
 
 /* Variáveis globais*/
-static int keys_dev = -1;
+static int keys_dev;
 
 /*Protótipos das funções*/
 int KEYS_open();
-int KEYS_read(char *button);
+int KEYS_read(char* button);
 int KEYS_close();
 
 
-int KEYS_open(){
-  /* abrir o arquivo com permissão de escrita e caso exista, sobreescreve o arquivo */
+int
+KEYS_open(){
+  keys_dev = -1;
+  /* Abre o arquivo com permissão de escrita*/
     keys_dev = open(KEYS_DRIVER_PATH, O_RDONLY);
     if (keys_dev == -1) {
         printf("Failed to open file!\n");
@@ -34,14 +36,20 @@ int KEYS_open(){
   return 0;
 }
 
-int KEYS_read(char *button){
+int
+KEYS_read(char *button){
+  /* Realiza a leitura dos botões*/
   int ret = read(keys_dev, button, 1);
-  return ret; //0 if success
-
+  if(ret != 0 || *button == NULL){
+    printf("Failed to read keys!\n");
+    return -1;
+  }
+  return 0; 
 }
 
-int KEYS_close(){
-  /* caso haja algum erro ao encerrar a comunicação retorna -1 */ 
+int
+KEYS_close(){
+  /* Caso haja algum erro ao encerrar a comunicação, retorna -1 */ 
   if (close(keys_dev) == -1) {
       printf("Failed to close file!\n");
       return -1;
