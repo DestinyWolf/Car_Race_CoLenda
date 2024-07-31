2d3069a530c6df91c1f747a8a96829115ff16ce6
Brenda <1brendabarbosa@gmail.com>
2024-07-30 21:00:36 -0300

    feat: 4 and restart sprites

--- source/Testes/offset_sprite.h
+++ source/Testes/offset_sprite.h
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "../Lib/colenda.h"
+
 #define CONE                0
 #define SINGLE_WOOD         1
 #define DOUBLE_WOOD         2
@@ -21,5 +27,17 @@
 #define N1                  20
 #define N2                  21
 #define N3                  22
-#define X                   23
-#define JOYSTICK            24
+#define N4                  23
+#define RESTART_SIMBOL      24
+#define X_SIMBOL            25
+#define JOYSTICK            26
+#define A 27 
+#define C 28
+#define D 29
+#define E 30
+#define N 31
+#define P 32
+#define R 33
+#define S 34
+#define U 35
+#define V 36
